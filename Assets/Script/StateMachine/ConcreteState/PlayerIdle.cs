c175f21a21f2889decb7b79c0a108bb7f264d4eb
ADMIN\Admin <emokiller22101993@gmail.com>
2024-01-30 09:22:35 +0700

    no message

---
+++ Assets/Script/StateMachine/ConcreteState/PlayerIdle.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdle : State<Player>
+{
+    public PlayerIdle(Player character, StateMachine<Player> characterStateMachine) : base(character, characterStateMachine)
+    {
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+    public override void FrameUpdate()
+    {
+        base.FrameUpdate();
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameUtilities.ScreenRayCastOnWorld(character.MoveDestination);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            character.MoveDestination(GameUtilities.MousePositionInWorld());
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            character.DashMoveDirection(character.transform.position + character.directionTarget.localPosition * 5);
+            character.agent.SetSpeed(15);
+        }
+        if (character.Horizontal != 0 || character.Vertical != 0)
+        {
+            character.agent.MoveToDirection(new Vector3(character.Horizontal, 0, character.Vertical));
+            character.directionTarget.localPosition = new Vector3(character.Horizontal, 0, character.Vertical).normalized;
+
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
---
+++ Assets/Script/StateMachine/Enemy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+public class Enemy : CharacterBrain
+{
+    
+}
